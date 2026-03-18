f7e1d7018c0d87f5ee5aba4d4c52325a6e116827
JLR1959 <jlouis40@hotmail.com>
2026-03-17 23:56:55 -0400

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.vpijlr.logiciel',
-  appName: 'VPIJLR',
-  webDir: 'www'
-};
-
-export default config;
